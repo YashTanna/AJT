4d7869f343c3534cd7a4f064e33d24fa7c47d9c2
YASH TANNA <yashtannasvkd@gmail.com>
2024-08-26 16:13:14 +0530

    Initial Commit

---
+++ WebApplication1/src/java/com/yash/MyServlet.java
@@ -0,0 +1,39 @@
+package com.yash;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class MyServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String uname = request.getParameter("username");
+        String pass = request.getParameter("password");
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>NewServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h3>Username : "+uname+" </h3>");
+            out.println("<h3>Password : "+pass+" </h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
